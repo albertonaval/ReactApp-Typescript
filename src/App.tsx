f330f002758e580463a2682ed5454d856262d5ee
albertonaval <albertonaval.dev@gmail.com>
2023-03-21 21:04:33 +0100

    Add axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,37 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import List from './components/List';
 import Form from './components/Form';
-import { Sub } from './types';
-import userEvent from '@testing-library/user-event';
-
-
-
+import { Sub, SubsResponseFromApi } from './types';
+import axios from 'axios';
 
 interface AppState {
   subs: Array<Sub>
   newSubsNumber: number
 }
 
-const INITIAL_STATE = [
-      {
-      nick: 'coscorron',
-      subMonths: 3,
-      avatar: 'https://i.pravatar.cc/150?u=coscorron',
-      description: 'Coscorron es buena gente'
-    },
-    {
-      nick: 'Gofio',
-      subMonths: 2,
-      avatar: 'https://i.pravatar.cc/150?u=gofio',
-    }
-]
-
 function App() {
   const [subs, setSubs] = useState<Sub[]>([])
   const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0)
   const divRef = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    setSubs(INITIAL_STATE)
-  }, [])
-
   const handleNewSub = (newSub: Sub): void => {
-  setSubs(subs => [...subs, newSub])
-}
+    setSubs(subs => [...subs, newSub])
+    setNewSubsNumber(n => n + 1)
+  }
 
+  useEffect(() => {
+    const fetchSubs = async (): Promise<SubsResponseFromApi> => {
+      const response = await axios
+        .get('http://locahost:3000/subs');
+      return response.data;
+    }
+  }, [])
 
   return (
     <div className="App" ref={divRef} >
       <h1>SUBSCRISTORES</h1>
       <List subs={subs} />
+      New Subs: {newSubsNumber}
       <Form onNewSub={handleNewSub} />
     </div>
   );
